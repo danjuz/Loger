e2fb73b97dee4080e026d0b9797c889a960b25bd
Juzdan <daniel.juzovitskij@gmail.com>
2015-08-22 23:36:07 +0200

    Added js for date and month

--- app/scripts/coffeescript/main.coffee
+++ app/scripts/coffeescript/main.coffee
@@ -1,5 +1,13 @@
-daniel = ->
-  1+1
-  console.log "Everything will be allright"
+dateNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']
 
-daniel()
+dateOfToday = ->
+  today = new Date()
+  date = today.getDate()
+  month = today.getMonth()
+
+  dateLocation = document.querySelector('.header__date-area')
+  
+  #dateLocation.innerHTML = "<div class='date-area__date'>" + date + "</div><br/><div class='date-area__month'>" + dateNames[month] + "</div>"
+  dateLocation.innerHTML = date + '<br>' + dateNames[month]
+
+dateOfToday()
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,17 @@
 (function() {
-  var daniel;
+  var dateNames, dateOfToday;
 
-  daniel = function() {
-    1 + 1;
-    return console.log("Everything will be allright");
+  dateNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
+
+  dateOfToday = function() {
+    var date, dateLocation, month, today;
+    today = new Date();
+    date = today.getDate();
+    month = today.getMonth();
+    dateLocation = document.querySelector('.header__date-area');
+    return dateLocation.innerHTML = date + '<br>' + dateNames[month];
   };
 
-  daniel();
+  dateOfToday();
 
 }).call(this);

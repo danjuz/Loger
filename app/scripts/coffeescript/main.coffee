47b1e625536e585233e7ed449720fdd01a2f98d6
Juzdan <daniel.juzovitskij@gmail.com>
2015-08-24 12:11:18 +0200

    created facebook login and structures CS to object form

--- app/scripts/coffeescript/main.coffee
+++ app/scripts/coffeescript/main.coffee
@@ -1,12 +1,37 @@
 dateNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']
 
-dateOfToday = ->
-  today = new Date()
-  date = today.getDate()
-  month = today.getMonth()
+logerApp = {
 
-  dateLocation = document.querySelector('.header__date-area')
+  loginButton: document.querySelector('.register-login-container__button')
 
-  dateLocation.innerHTML = "<div class='date-area__date'>" + date + "</div><br/><div class='date-area__month'>" + dateNames[month] + "</div>"
+  init: ->
+    this.bindEvents()
 
-dateOfToday()
+  bindevents: ->
+    self = this
+    this.loginButton.addEventListener 'click', ->
+      self.redirect()
+    this.dateOfToday()
+
+  dateOfToday: ->
+    today = new Date()
+    date = today.getDate()
+    month = today.getMonth()
+
+    dateLocation = document.querySelector('.header__date-area')
+    dateLocation.innerHTML = "<div class='date-area__date'>" + date + "</div><br/><div class='date-area__month'>" + dateNames[month] + "</div>"
+
+  redirect: ->
+    ref = new Firebase('https://loger.firebaseio.com')
+    ref.authWithOAuthPopup 'facebook', (error, authData) ->
+      if error
+        console.log 'Login Failed!', error
+      else
+        console.log 'Authenticated successfully with payload:', authData
+        window.location.href = 'http://www.localhost:9000/logg-results.html';
+
+    return
+
+
+  }
+logerApp.bindevents()
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,43 @@
 (function() {
-  var dateNames, dateOfToday;
+  var dateNames, logerApp;
 
   dateNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
 
-  dateOfToday = function() {
-    var date, dateLocation, month, today;
-    today = new Date();
-    date = today.getDate();
-    month = today.getMonth();
-    dateLocation = document.querySelector('.header__date-area');
-    return dateLocation.innerHTML = "<div class='date-area__date'>" + date + "</div><br/><div class='date-area__month'>" + dateNames[month] + "</div>";
+  logerApp = {
+    loginButton: document.querySelector('.register-login-container__button'),
+    init: function() {
+      return this.bindEvents();
+    },
+    bindevents: function() {
+      var self;
+      self = this;
+      this.loginButton.addEventListener('click', function() {
+        return self.redirect();
+      });
+      return this.dateOfToday();
+    },
+    dateOfToday: function() {
+      var date, dateLocation, month, today;
+      today = new Date();
+      date = today.getDate();
+      month = today.getMonth();
+      dateLocation = document.querySelector('.header__date-area');
+      return dateLocation.innerHTML = "<div class='date-area__date'>" + date + "</div><br/><div class='date-area__month'>" + dateNames[month] + "</div>";
+    },
+    redirect: function() {
+      var ref;
+      ref = new Firebase('https://loger.firebaseio.com');
+      ref.authWithOAuthPopup('facebook', function(error, authData) {
+        if (error) {
+          return console.log('Login Failed!', error);
+        } else {
+          console.log('Authenticated successfully with payload:', authData);
+          return window.location.href = 'http://www.localhost:9000/logg-results.html';
+        }
+      });
+    }
   };
 
-  dateOfToday();
+  logerApp.bindevents();
 
 }).call(this);
